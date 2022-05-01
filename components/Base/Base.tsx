8d015fd292f034ddf9d7cd4680a6949cdb905376
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2022-05-01 00:59:28 -0400

    add cool postcard view

--- components/Base/Base.tsx
+++ components/Base/Base.tsx
@@ -8,7 +8,7 @@ type BaseProps = PropsWithChildren<{
 export default function Base({ children }: BaseProps) {
   return (
     <>
-      <div className="prose-a:text-sky-500 prose-a:no-underline prose-a:transition hover:prose-a:text-sky-600">
+      <div className="prose-a:text-sky-500 prose-a:no-underline prose-a:transition hover:prose-a:text-sky-600 dark:bg-zinc-900 dark:bg-none dark:prose-a:text-zinc-300 dark:hover:prose-a:text-sky-600 dark:prose-headings:text-slate-200 dark:prose-strong:text-zinc-300 dark:prose-blockquote:text-zinc-300">
         <div className="max-w-[700px] mx-auto flex flex-col min-h-screen px-8 sm:px-4 prose">
           <nav className="flex space-x-5 mt-5 mb-10">
             <Link href="/">
@@ -18,7 +18,9 @@ export default function Base({ children }: BaseProps) {
               <a aria-label="Posts">Posts</a>
             </Link>
           </nav>
-          <main className="text-gray-700 flex-grow">{children}</main>
+          <main className="text-gray-700 prose flex-grow dark:text-zinc-400">
+            {children}
+          </main>
           <div className="my-5 pt-5 border-t-2 border-dotted border-t-slate-600">
             Created by{" "}
             <a target="_blank" rel="noreferrer" href="https://devkanisk.com">
--- components/PostCard/PostCard.tsx
+++ components/PostCard/PostCard.tsx
@@ -10,22 +10,22 @@ export default function PostCard({ post }: PostCardProps) {
   return (
     <Link passHref key={`posts/${post.slug}`} href={`posts/${post.slug}`}>
       <a
-        className="relative block p-8 overflow-hidden border border-gray-100 rounded-lg mt-2"
+        className="relative block p-8 overflow-hidden border border-gray-100 rounded-lg mt-6"
         aria-label={post.title}
       >
         <span className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
 
         <div className="justify-between sm:flex">
           <div>
-            <h5 className="text-xl font-bold text-gray-900">{post.title}</h5>
-            <p className="mt-1 text-xs font-medium text-gray-600">
+            <h5 className="text-xl font-bold text-sky-400">{post.title}</h5>
+            <p className="mt-1 text-xs font-medium text-gray-600 dark:text-zinc-400">
               By Kanisk Chakraborty
             </p>
           </div>
 
           <div className="flex-shrink-0 hidden ml-2 sm:block">
             <Image
-              className="object-cover w-16 h-16 rounded-lg shadow-sm"
+              className="object-cover w-16 h-16 rounded-full shadow-sm dark:bg-zinc-400"
               src="/static/dk-small.svg"
               width={64}
               height={64}
@@ -35,17 +35,23 @@ export default function PostCard({ post }: PostCardProps) {
         </div>
 
         <div className="mt-0 sm:pr-8">
-          <p className="text-sm text-gray-500">{post.description}</p>
+          <p className="text-sm text-gray-500 dark:text-zinc-500">
+            {post.description}
+          </p>
         </div>
 
         <dl className="flex mt-2">
           <div className="flex flex-col-reverse">
-            <dt className="text-sm font-medium text-gray-600">Published</dt>
+            <dt className="text-sm font-medium text-gray-600 dark:text-zinc-400">
+              Published
+            </dt>
             <dd className="text-xs text-gray-500">{post.publishedAt}</dd>
           </div>
 
           <div className="flex flex-col-reverse ml-3 sm:ml-6">
-            <dt className="text-sm font-medium text-gray-600">Reading Time</dt>
+            <dt className="text-sm font-medium text-gray-600 dark:text-zinc-400">
+              Reading Time
+            </dt>
             <dd className="text-xs text-gray-500">
               {post.readingTime
                 ? `${post.readingTime.text.substring(
