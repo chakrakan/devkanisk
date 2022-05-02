328796d2aa203f00719703518b9ecce3a43b1b47
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2022-05-02 05:24:29 -0400

    fix tag for light mode

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -4,7 +4,7 @@ type TagProps = {
 
 export default function Tag({ title }: TagProps) {
   return (
-    <span className="mx-0.5 font-normal bg-zinc-700 hover:bg-zinc-400 rounded-md hover:text-zinc-50 px-2 text-sm leading-loose cursor-pointer">
+    <span className="mx-0.5 font-normal bg-zinc-200 hover:bg-zinc-400 dark:bg-zinc-700 dark:hover:bg-zinc-400 rounded-md dark:hover:text-zinc-50 px-2 text-sm leading-loose cursor-pointer">
       {title}
     </span>
   );
